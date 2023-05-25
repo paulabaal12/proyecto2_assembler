562b468a7185c698ef4d90cf33de74cc4b16ff5c
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2023-05-25 11:17:02 -0600

    Update proyecto_assembler.asm

---
+++ proyecto_assembler.asm
@@ -0,0 +1,13 @@
+; -----------------------------------------------
+; UNIVERSIDAD DEL VALLE DE GUATEMALA 
+; Organización de computadoras y Assembler
+; Ciclo 1 - 2023
+; Nombre: proyecto_assembler.asm
+; Descripción: 
+; Autor: 
+; ----------------------------------------------- 
+
+.386
+.model flat, stdcall, c
+.stack 4096
+.data
