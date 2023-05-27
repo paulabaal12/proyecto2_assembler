e1798b4b9fc78faf4de1427b91cbcd737f5cffec
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2023-05-27 17:06:45 -0600

    preguntitas :)

--- proyecto_assembler.asm
+++ proyecto_assembler.asm
@@ -13,7 +13,20 @@
 ;ExitProcess proto,dwExitCode:dword
 
 .data
-msg1 BYTE 'Bienvenidos!',0Ah,0
+msg0 BYTE 'Bienvenidos!, 0Ah, 0
+preg1 BYTE '¿Cuánto es | -77 | ?', 0Ah, 0
+preg2 BYTE '¿16 + 32 es igual a?', 0Ah, 0
+preg3 BYTE 'raíz cuadrada de 144', 0Ah, 0
+preg4 BYTE '¿Cuánto es e a la 0?', 0Ah, 0
+preg5 BYTE '¿Cuanto es cos(90º)?', 0Ah, 0
+preg6 BYTE 'La derivada de 5x es', 0Ah, 0
+preg7 BYTE '( 5 + 2) * 10 / 2 es', 0Ah, 0
+preg8 BYTE '¿ Cuánto es 56 / 7 ?', 0Ah, 0
+preg9 BYTE 'si (x*2)/3 = 10, x es', 0Ah, 0
+preg10 BYTE '6x8 menos cuatro es:', 0Ah, 0
+preg11 BYTE '1/3 de 66 es igual a', 0Ah, 0
+preg12 BYTE 'si 10+x = 15, 2x es:', 0Ah, 0
+preg13 BYTE 'x+y=1 y 3x+2y=6, x =', 0Ah, 0
 
 .code
 includelib libucrt.lib
